bf0da536ea999d3531ee11e1a5a1467cbbbb143c
Mir__off__Shoh <98812035+pubgmobileplay@users.noreply.github.com>
2022-05-04 11:14:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,95 @@
+var item  = document.querySelector(".header__links")
+item.addEventListener("mouseover", function () {
+    item.style.color = "red"
+
+})
+item.style.color = "#000"
+item.addEventListener("mouseout", function () {
+    item.style.color = "#000"
+})
+
+var items  = document.querySelector(".header__links1")
+items.addEventListener("mouseover", function () {
+    items.style.color = "red"
+})
+items.style.color = "#000"
+items.addEventListener("mouseout", function () {
+    items.style.color = "#000"
+})
+var items1  = document.querySelector(".header__links2")
+items1.addEventListener("mouseover", function () {
+    items1.style.color = "red"
+})
+items1.style.color = "#000"
+items1.addEventListener("mouseout", function () {
+    items1.style.color = "#000"
+})
+
+
+var item1  = document.querySelector(".content__link")
+item1.addEventListener("mouseover", function () {
+    item1.style.background = "#FF6464"
+    item1.style.color = "#fff"
+})
+item1.style.color = "#000"
+item1.addEventListener("mouseout", function () {
+    item1.style.background = "#fff"
+    item1.style.color = "#000"
+})
+var item2  = document.querySelector(".header__nav")
+item2.style.background = "#EDF7FA"
+
+var item3  = document.querySelector(".section__text")
+item3.style.color = "#21243D"
+
+var item4  = document.querySelector(".section__text1")
+item4.style.color = "#21243D"
+
+var item5  = document.querySelector(".section__text2")
+item5.style.color = "#21243D"
+
+var item6  = document.querySelector(".content__text")
+item6.style.color = "#000"
+
+var item7  = document.querySelector(".section__text3")
+item7.style.color = "#21243D"
+
+var item8  = document.querySelector(".section1__text")
+item8.style.color = "#21243D"
+
+var item9  = document.querySelector(".section1__text1")
+item9.style.color = "#21243D"
+
+var items2  = document.querySelector(".section1__text3")
+items2.style.color = "#8695A4"
+
+var items3  = document.querySelector(".section1__text2")
+items3.style.color = "#fff"
+items3.style.background = "#142850" 
+
+items3.style.background = "#142850"
+var items3  = document.querySelector(".section1__text5")
+items3.style.color = "#fff"
+items3.style.background = "#142850"
+
+var items9  = document.querySelector(".section1__text6")
+items9.style.color = "#fff"
+items9.style.background = "#142850"
+
+var items3  = document.querySelector(".section1__text4")
+items3.style.color = "#21243D"
+
+var items4  = document.querySelector(".section__link")
+items4.style.color = "#00A8CC"
+
+var items4  = document.querySelector(".section")
+items4.style.background = "#EDF7FA"
+
+var items5  = document.querySelector(".section__div2")
+items5.style.background = "#fff"
+
+var items6  = document.querySelector(".section__div21")
+items6.style.background = "#fff"
+
+var items7  = document.querySelector(".section1__text3")
+items7.style.color = "#8695A4"
